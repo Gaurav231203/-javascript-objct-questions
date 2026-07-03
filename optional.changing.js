0f1fbf4dd23fe3ac2963facc61f4c07b2237d8a9
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-03 15:03:54 +0530

    Added new files

---
+++ optional.changing.js
@@ -0,0 +1,17 @@
+//what will happen if cordinates are missing and how will u prevent errors.
+// by using (optional changing mathod we can run a code without errror ),
+
+
+let info={
+    name:"gaurav",
+    branch:"cse",
+    addrss:{
+        city:"kashipur",
+
+          
+
+        
+        
+    },
+};
+console.log(info.addrss?.codrinates);
