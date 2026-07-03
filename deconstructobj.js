7d0367b72c7d8a77e37b94d72bc6b4a71808d9c7
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-03 15:25:19 +0530

    Added new files

---
+++ deconstructobj.js
@@ -0,0 +1,22 @@
+//what will happen if cordinates are missing and how will u prevent errors.
+// by using (optional changing mathod we can run a code without errror ),
+
+
+let info={
+    name:"gaurav",
+    branch:"cse",
+    address:{
+        city:"kashipur",
+        codrinates:{
+            lat:22.3,
+            lng:21.3,
+        }
+    },
+};
+console.log(info);
+//destructure the  city and lat from laocation object above
+
+let{city}= info.address;
+let {lat}=info.address.codrinates;
+console.log(city);
+console.log(lat);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+let {lat}=info.address.codrinates.lat;
