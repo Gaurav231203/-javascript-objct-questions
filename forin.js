ec515cda2b6dfbbd9409fd2f47c58b0584899a96
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-04 00:54:09 +0530

    Added new files

---
+++ forin.js
@@ -0,0 +1,11 @@
+//usee for in loop to log all object
+let stud ={
+    name:"gaurav",
+    age:22,
+    isEnrolled:"true",
+};
+for( let key in stud){//ye line jarur yaad honin chye
+
+    console.log(key,":",stud[key]);// ye line yaad honi chye 
+
+}
